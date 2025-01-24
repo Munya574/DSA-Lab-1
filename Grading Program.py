0b54b707296797f87d50caeaad7dcfff260eefb8
ChilawoM Munene <cmunene@gsumail.gram.edu>
2025-01-23 21:43:20 -0600

    updates

--- Grading Program.py
+++ Grading Program.py
@@ -1,4 +1,21 @@
+students = []
+grades = []
 category = []
+
+Max_students = 10
+
+from tabulate import tabulate
+
+count = 0 
+while count < 10:
+    s = input('Enter name(Enter "cancel" to stop): ')
+    if s == "cancel":
+        break
+    g = input('Enter grade: ')
+    students.append(s)
+    grades.append(g)
+    count += 1
+
 for i in range(len(grades)):
     if grades[i] >= 90:
         category.append('Excellent')
@@ -8,3 +25,8 @@ for i in range(len(grades)):
         category.append('Avergae')
     else:
         category.append('Needs Improvement')
+print(category)
+
+data = [students, grades, category]
+header = ['Name', 'Grade', 'Category']
+print(tabulate(data, headers=header, tablefmt="pretty"))
