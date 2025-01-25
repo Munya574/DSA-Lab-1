1d4d2973cb974f114313ad6270c1426002fe50d5
Grace5190 <gngoma@gsumail.gram.edu>
2025-01-24 22:29:24 -0600

    added a few validations under "assignment 1.py"

--- assignment 1.py
+++ assignment 1.py
@@ -1,14 +1,23 @@
 from tabulate import tabulate
 
-count = 0 
-while count < 10:
+while len(students) < max_students:
     s = input('Enter name(input "cancel" to break): ')
     if s == "cancel":
         break
-    g = eval(input('Enter grade: '))
-    students.append(s)
-    grades.append(g)
-    count += 1
+    if not name:
+        print("Name cannot be empty.")
+        continue
+    
+    try:
+        g = eval(input('Enter grade: '))
+        if 0 <= g <= 100:
+            students.append(s)
+            grades.append(g)
+        else:
+            print("Grade must be between 0 and 100.")
+    except ValueError:
+        print(" Invalid input")
+        
 
 
 data = [students, grades, category]
