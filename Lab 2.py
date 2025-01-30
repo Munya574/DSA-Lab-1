c890c2d30d027b98f1fa79ccf10b7eaba8109d75
ChilawoM Munene <cmunene@gsumail.gram.edu>
2025-01-30 13:34:11 -0600

    lab 2

---
+++ Lab 2.py
@@ -0,0 +1,22 @@
+item = []
+prices =[]
+max_total = 100
+balance = 100 - sum(prices)
+price_count = 0
+while sum(prices) < max_total:
+    item_name = input('Enter item name(input "cancel" to quit): ')
+    if item_name.lower() == "cancel":
+        break
+    if not item_name:
+        print("Name cannot be empty.")
+        continue
+    try:
+        price = float(input('Enter price: '))
+        price_count += calculate_tax(price)
+        if price_count > max_total:
+            item.append(item_name)
+            prices.append(price)
+        else:
+            print("Price must be greater than 0. Re-enter item name and price")
+    except ValueError:
+        print(" Invalid input. Re-enter item name and price")
