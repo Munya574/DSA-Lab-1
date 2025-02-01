27ead31b4a379439cd92b5aee1facc6f513e8c6a
Grace5190 <gngoma@gsumail.gram.edu>
2025-01-31 19:50:39 -0600

    blah

--- Lab 2.py
+++ Lab 2.py
@@ -1,26 +1,39 @@
 def calculate_tax(price):
     tax_rate = 10.44 / 100
     return price * tax_rate
+
 item = []
-prices =[]
+prices = []
 max_total = 100
-balance = 100 - sum(prices)
-price_count = 0
-while sum(prices) < max_total:
-    item_name = input('Enter item name(input "cancel" to quit): ')
+balance = max_total
+
+while balance > 0:
+    item_name = input('Enter item name (input "cancel" to quit): ')
     if item_name.lower() == "cancel":
         break
     if not item_name:
         print("Name cannot be empty.")
         continue
+
     try:
         price = float(input('Enter price: '))
-        price_count += calculate_tax(price)
-        if price_count > max_total:
-            
+        if price <= 0:
+            print("Price must be greater than 0. Re-enter item name and price")
+            continue
+
+        total_price = price + calculate_tax(price)
+
+        if balance - total_price >= 0:
             item.append(item_name)
             prices.append(price)
+            balance -= total_price
+            print(f" Your remaining balance is ", round(balance, 2))
         else:
-            print("Price must be greater than 0. Re-enter item name and price")
+            print("You have exceeded your budget. Your remaining balance is $", round(balance, 2))
+            wants = input("Would you like to continue shopping within your budget? (yes/no): ")
+            if wants.lower() != "yes":
+                break
+
     except ValueError:
-        print(" Invalid input. Re-enter item name and price")
+        print("Invalid input. Re-enter item name and price.")
+
